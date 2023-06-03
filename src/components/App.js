6316f1cced8683cd6b1ac4018ee35a279d87206c
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-06-04 00:44:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../styles/App.css';
+
 const App = () => {
+  
   const handleClick = (event) =>{
     console.log('Button id is:-'+event.target.id);
   }
+  
   return (
     <div id="main">
       <button id="button-a" onClick={handleClick}>Button A</button>
