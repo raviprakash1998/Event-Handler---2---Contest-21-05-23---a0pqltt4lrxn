83262ae00578bf56edf834a80a1bac63296da3be
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-06-04 00:36:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-
   const handleClick = (event) =>{
-    // use console.log
     console.log('Button id is:-'+event.target.id);
-
   }
-
-  // do not remove the two buttons or change their id
   return (
     <div id="main">
       <button id="button-a" onClick={handleClick}>Button A</button>
